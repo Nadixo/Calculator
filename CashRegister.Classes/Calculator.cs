5d5a3da17b544227c147f0d70d5b13d1e6b3dcb1
Nadixo <75481929+Nadixo@users.noreply.github.com>
2022-09-07 15:34:37 +0200

    Lars

--- CashRegister.Classes/Calculator.cs
+++ CashRegister.Classes/Calculator.cs
@@ -2,29 +2,79 @@
 {
     public class Calculator
     {
-
+        private double item = 0;
 
         public double Add(double a, double b)
         {
-            double item = a + b;
+            item = a + b;
             return item;
         }
         public double Subtract(double a, double b)
         {
-            double item = a - b;
+            item = a - b;
             return item;
         }
         public double Multiply(double a, double b)
         {
-            double item = a * b;
+            item = a * b;
             return item;
         }
         public double Power(double x, double exp)
         {
-            double item = Math.Pow(x, exp);
+            item = Math.Pow(x, exp);
             return item;
         }
 
+        public double Divide(double dividend, double divisor)
+        {
+            item = dividend / divisor;
+            return item;
+        }
+        public double Accumulator
+        {
+            get
+            {
+                return item;
+            }
+            private set
+            {
+                item = value;
+            }
+        }
 
+        public void Clear()
+        {
+            item = 0;
+        }
+
+        public double Add(double addend)
+        {
+            item += addend;
+            return item;
+        }
+
+        public double Subtract(double subtractor)
+        {
+            item -= subtractor;
+            return item;
+        }
+
+        public double Multiply(double multiplier)
+        {
+            item *= multiplier;
+            return item;
+        }
+
+        public double Divide(double divisor)
+        {
+            item /= divisor;
+            return item;
+        }
+
+        public double Power(double exponent)
+        {
+            item = Math.Pow(item, exponent);
+            return item;
+        }
     }
 }
